33ebb4844152a7ff08f3cac8f7f692d183c73b85
Zhdzd <zahidabdullah18@gmail.com>
2022-01-05 00:59:16 +0800

    First commit

---
+++ src/main/java/com/example/Day11Workshop/Day11WorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Day11Workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day11WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Day11Workshop/Day11WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Day11Workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
